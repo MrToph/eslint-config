25d614883ed4ed2d88db0e8470e3cb879411a36c
Christoph Michel <christoph.michel.1992@gmail.com>
2018-05-28 23:18:35 +0200

    Fix plugin:vue module not found error by not require.resolving it

--- vue.js
+++ vue.js
@@ -9,9 +9,8 @@ module.exports = {
     browser: true,
     commonjs: true
   },
-  extends: ['./index.js', 'plugin:vue/recommended'].map(
-    require.resolve
-  ),
+  // 'plugin:vue/recommended' may _not_ be require.resolved
+  extends: [require.resolve('./index.js'), 'plugin:vue/recommended'],
   // required to lint *.vue files
   plugins: [
     'vue'
